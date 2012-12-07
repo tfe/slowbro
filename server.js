1415379661f5f225833f2662a7b1b2b765a6766e
Todd Eichel <todd@toddeichel.com>
2012-12-07 01:40:03 -0500

    Guard against NaNs in delay. Also add in a little entropy to the delay time.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http');
 http.createServer(function (req, res) {
   
-  var delay = parseInt(req.url.substr(1, req.url.length));
+  var delay = parseInt(req.url.substr(1, req.url.length)) || 0;
+  console.log(delay);
+  
+  // add some entropy
+  
+  delay += Math.floor(Math.random()*parseInt(delay*0.2)) // add 20% more
+  console.log(delay);
+  
   res.writeHead(200, {'Content-Type': 'text/plain'});
   
   setTimeout(function () {
